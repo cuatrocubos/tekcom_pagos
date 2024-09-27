8ca16d461b25f197029ff028de39fa0cb5817e55
Jorge Giron <jorgefgiron@Jorges-MacBook-Air.local>
2024-09-27 08:37:25 -0600

    feat: firma digital

---
+++ tekcom_pagos/solicitudes_de_pagos/doctype/firma_digital/firma_digital.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Cuatrocubos Soluciones and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Firma Digital", {
+// 	refresh(frm) {
+
+// 	},
+// });
