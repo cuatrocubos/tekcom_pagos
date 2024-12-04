e04e284587faca2fd98931ae234d4f08a4cc5fd1
Jorge Girón <jorge.giron.29@gmail.com>
2024-12-04 12:05:36 -0600

    fix: custom_fiels

--- tekcom_pagos/patches/v15_0/set_default_currency_exchange_rate_solicitud_viaticos.py
+++ tekcom_pagos/patches/v15_0/set_default_currency_exchange_rate_solicitud_viaticos.py
@@ -5,5 +5,5 @@ def execute():
   frappe.reload_doc('Viaticos','doctype','Solicitud de Viaticos')
   solicitud_de_viaticos = frappe.qb.DocType("Solicitud de Viaticos")
 
-  frappe.qb.update(solicitud_de_viaticos).set(solicitud_de_viaticos.currency,"HNL").where(solicitud_de_viaticos.currency.is_null()).run()
+  frappe.qb.update(solicitud_de_viaticos).set(solicitud_de_viaticos.currency,"HNL").where(solicitud_de_viaticos.currency.isnull()).run()
   
